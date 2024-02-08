8fc0999a4e65630c6a56e677d5443052f4e4e610
arturo <artmcel@hotmail.com>
2024-02-07 21:44:59 -0600

    set value = n y add onChange

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,25 +1,32 @@
+import { useState } from "react"
+
 export const FormComponent = () => {
-  return (
-    <form action="">
-        <div className="mb-3">
-            <label htmlFor="number" className="form-label">Ingresa un valor</label>
-            <input
-                type="text"
-                className="form-control"
-                name="number"
-                id="number"
-                aria-describedby="helpId"
-                placeholder="ingresa un valor"
-            />
-        </div>
-        <button
-            type="submit"
-            className="btn btn-primary"
-        >
-            Resolver
-        </button>
+
+    const [n, setN] = useState(0);
+
+    return (
+        <form action="">
+            <div className="mb-3">
+                <label htmlFor="number" className="form-label">Ingresa un valor</label>
+                <input
+                    type="number"
+                    className="form-control"
+                    name="number"
+                    id="number"
+                    value={n}
+                    onChange={ (e)=>setN(e.target.value) }
+                    aria-describedby="helpId"
+                    placeholder="ingresa un valor"
+                />
+            </div>
+            <button
+                type="submit"
+                className="btn btn-primary"
+            >
+                Calcular
+            </button>
+            
+        </form>
         
-    </form>
-    
-  )
+    )
 }
