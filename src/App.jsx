69077b026cf732b86d561fc8e372cfabd715a142
arturo <artmcel@hotmail.com>
2024-02-08 17:50:56 -0600

    add col-md

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <main className="container">
       <section className="row justify-content-center mt-5">
-        <div className="col">
+        <div className="col col-md-6">
           <h1>Dev Training Sngular</h1>
           <FormComponent/>
         </div>
