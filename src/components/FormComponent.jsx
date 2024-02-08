82b7866d99c167716712bc3d9d46e0483a30733d
arturo <artmcel@hotmail.com>
2024-02-07 21:52:33 -0600

    add function handleSubmit

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,32 +1,49 @@
 import { useState } from "react"
+import { serie } from '../utils/Serie'
 
 export const FormComponent = () => {
 
     const [n, setN] = useState(0);
+    const [result, setResult] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try {
+            setResult( serie(n) );
+        } catch (error) {
+            setResult( error.message );
+        }
+
+    }
 
     return (
-        <form action="">
-            <div className="mb-3">
-                <label htmlFor="number" className="form-label">Ingresa un valor</label>
-                <input
-                    type="number"
-                    className="form-control"
-                    name="number"
-                    id="number"
-                    value={n}
-                    onChange={ (e)=>setN(e.target.value) }
-                    aria-describedby="helpId"
-                    placeholder="ingresa un valor"
-                />
+        <>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="number" className="form-label">Ingresa un valor</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        name="number"
+                        id="number"
+                        value={n}
+                        onChange={ (e)=>setN(e.target.value) }
+                        aria-describedby="helpId"
+                        placeholder="ingresa un valor"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="btn btn-primary"
+                >
+                    Calcular
+                </button>
+                
+            </form>
+            <div className="col">
+                { result && <p>Resultado: {result}</p>}
             </div>
-            <button
-                type="submit"
-                className="btn btn-primary"
-            >
-                Calcular
-            </button>
-            
-        </form>
+        </>
         
     )
 }
